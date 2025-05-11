dc8be27e0e0f3dc38c6684e2e8d0885484639f2f
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-05-11 09:47:51 +0100

    feat(structs.h) add template struct for argument handing

---
+++ project/includes/structs.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   structs.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ajelloul <ajelloul@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/11 09:25:15 by ajelloul          #+#    #+#             */
+/*   Updated: 2025/05/11 09:36:20 by ajelloul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STRUCTS_H
+# define STRUCTS_H
+
+typedef struct s_cmd
+{
+	char	*man_cmd;
+	char	**options;
+	int		infile;
+	int		outfile;
+}	t_cmd;
+
+#endif

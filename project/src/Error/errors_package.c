4a0c17aa3fc23e11c1b2784ca59c1f8349dc6995
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-05-19 10:02:00 +0100

    feat (error) Add : export_error function

---
+++ project/src/Error/errors_package.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   errors_package.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ajelloul <ajelloul@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/19 09:34:18 by ajelloul          #+#    #+#             */
+/*   Updated: 2025/05/19 10:00:11 by ajelloul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "../../includes/minishell.h"
+
+/*
+** Handles errors and sets the exit status
+** @param msg: Error message to display
+** @param exit_status: Exit status to set
+** @param bash: Pointer to the minibash structure
+*/
+void	exit_with_error(const char *msg, int exit, t_minibash *bash)
+{
+	if (!bash)
+		return ;
+	bash->exit_status = exit;
+	perror(msg);
+}
+
+void	export_error(t_minibash *bash, char *content)
+{
+	ft_putstr_fd("minishell: export: `", 2);
+	ft_putstr_fd(content, 2);
+	ft_putendl_fd("`: not a valid identifier", 2);
+	bash->exit_status = 1;
+}

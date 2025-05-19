ca69f616c55ff8e3b68b97b43830fc096443bd68
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-05-19 10:43:47 +0100

    feat (includes/prototypes.h) Add export func  prototypes

--- project/includes/prototypes.h
+++ project/includes/prototypes.h
@@ -6,7 +6,7 @@
 /*   By: ajelloul <ajelloul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 12:24:29 by ajelloul          #+#    #+#             */
-/*   Updated: 2025/05/15 10:31:39 by ajelloul         ###   ########.fr       */
+/*   Updated: 2025/05/19 10:39:56 by ajelloul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,9 @@ t_status		echo(t_cmd *cmd);
 t_status		cd(t_cmd *cmd, t_minibash *info);
 void			pwd(t_minibash *bash);
 void			env(t_minibash *bash);
+void			builtin_exit(t_cmd *cmd, t_minibash *bash);
+void			unset(t_minibash *bash, char **args);
+void			export(t_minibash *bash, t_cmd *cmd);
 
 /* Environment */
 void			initialize_environment(t_minibash *info, char **env);
@@ -29,8 +32,13 @@ char			*extract_env_name(const char *str);
 char			*extract_env_value(const char *str);
 t_env			*create_env_node(char *data);
 t_env			*create_empty_env_node(char *name);
+void			remove_env_variable(t_env **env, char *var);
 
 /* Error && Exit*/
 void			exit_with_error(const char *msg, int exit, t_minibash *bash);
+void			export_error(t_minibash *bash, char *content);
+
+/*		EXPORT UTILS	*/
+void			display_exported_variable(t_minibash *bash);
 
 #endif

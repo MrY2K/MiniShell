9b51c8e38fc15d0fae30117a49cc3e450e94c667
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-06-11 13:42:46 +0100

    feat (Tokenizer) : fix norminette

--- project/src/tokenizer/syntax_middle.c
+++ project/src/tokenizer/syntax_middle.c
@@ -6,7 +6,7 @@
 /*   By: ajelloul <ajelloul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 12:17:12 by ajelloul          #+#    #+#             */
-/*   Updated: 2025/05/28 11:48:39 by ajelloul         ###   ########.fr       */
+/*   Updated: 2025/06/11 13:41:22 by ajelloul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,60 +15,81 @@
 int	is_redirection(t_token *cur_node)
 {
 	return ((cur_node)->type == '>' || (cur_node)->type == '<'
-		|| (cur_node)->type == TOKEN_REDIR_APPEND || (cur_node)->type == TOKEN_HEREDOC);
+		|| (cur_node)->type == TOKEN_REDIR_APPEND
+		|| (cur_node)->type == TOKEN_HEREDOC);
 }
 
 int	special_char(t_token *token)
 {
-	return ((token->type != ' ' && token->type != TOKEN_WORD && token->type != '$'
-		&& token->type != '\'' && token->type != '\"' && token->type != TOKEN_HEREDOC));
+	return ((token->type != ' ' && token->type != TOKEN_WORD
+			&& token->type != '$' && token->type != '\'' && token->type != '\"'
+			&& token->type != TOKEN_HEREDOC));
 }
+/*
+	Case 1: Normal Command Tokens //  we  Inside double
+		/  single quotes and we have alpha char echo "hello  world"
+	
+	case 2  We're inside quotes and token is a special character  
+		echo "value: $PATH"  echo 'hello $USER' echo 'hi | hej'
+	
+*/
 
 void	normal_cases(t_token **middle)
 {
 	if (!*middle)
 		return ;
-	// Case 1: Normal Command Tokens //  we  Inside double  /  single quotes and we have alpha char echo "hello  world"
 	if ((*middle) && special_char(*middle) && (*middle)->state != Normal)
 	{
-		while ((*middle) && (special_char(*middle) && (*middle)->state != Normal))
+		while ((*middle) && (special_char(*middle)
+				&& (*middle)->state != Normal))
 			(*middle) = (*middle)->next;
 	}
-	// case 2  We're inside quotes and token is a special character  echo "value: $PATH"  echo 'hello $USER' echo 'hi | hej'
-	else if (((*middle) != NULL) && !special_char(*middle) && (*middle)->state != Normal)
+	else if (((*middle) != NULL) && !special_char(*middle)
+		&& (*middle)->state != Normal)
 	{
-		while ((*middle) && !special_char(*middle) && (*middle)->state != Normal)
+		while ((*middle) && !special_char(*middle)
+			&& (*middle)->state != Normal)
 			(*middle) = (*middle)->next;
 	}
 	else if ((*middle) && (*middle)->state == Normal)
 		(*middle) = (*middle)->next;
 }
 
-int	check_middle_syntax(t_token **middle)
+/*
+	Case 1: Redirection operator found
+		Move past the redirection operator
+		Skip spaces after redirection
+		
+		echo hello >  echo hello > > file  echo hello > | 
+			grep  cat <<   cat <  echo hello >> < file   echo hello >>
+	
+	case 2 :
+		ls | |
+	
+		
+*/
+int	check_middle_syntax(t_token **middle, enum e_state NOR)
 {
 	while ((*middle))
 	{
 		skip_spaces(&(*middle));
-		// Case 1: Redirection operator found
-		if ((*middle) &&  is_redirection((*middle)) && (*middle)->state == Normal)
-		{
-			(*middle) = (*middle)->next; // Move past the redirection operator
-			skip_spaces(&(*middle)); // Skip spaces after redirection
-			if ((*middle) == NULL || ((is_redirection(*middle) || (*middle)->type == '|') && (*middle)->state == Normal))
-			{
-				// echo hello >  echo hello > > file  echo hello > | grep  cat <<   cat <  echo hello >> < file   echo hello >>
-				return (1);
-			}
-		}
-		else if ((*middle) && (*middle)->type == '|' && (*middle)->state == Normal) // ls | |
+		if ((*middle) && is_redirection((*middle))
+			&& (*middle)->state == Normal)
 		{
 			(*middle) = (*middle)->next;
-			skip_spaces(&(*middle)); // Skip spaces after redirection
-			// Another pipe
-			if (!(*middle) || ((*middle)->type == '|' && (*middle)->state == Normal))
-			{
+			skip_spaces(&(*middle));
+			if (!(*middle) || ((is_redirection(*middle)
+						|| (*middle)->type == '|') && (*middle)->state == NOR))
 				return (1);
-			}
+		}
+		else if ((*middle)
+			&& (*middle)->type == '|' && (*middle)->state == Normal)
+		{
+			(*middle) = (*middle)->next;
+			skip_spaces(&(*middle));
+			if (!(*middle)
+				|| ((*middle)->type == '|' && (*middle)->state == Normal))
+				return (1);
 		}
 		else
 			normal_cases(middle);

e1994bf15214aa5d2fe689c23279dfbe7c51473e
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-06-13 12:29:42 +0100

    env  Fix norminette ✅

--- project/src/env/env_manager.c
+++ project/src/env/env_manager.c
@@ -1,82 +1,18 @@
-// /* ************************************************************************** */
-// /*                                                                            */
-// /*                                                        :::      ::::::::   */
-// /*   env_manager.c                                      :+:      :+:    :+:   */
-// /*                                                    +:+ +:+         +:+     */
-// /*   By: ajelloul <ajelloul@student.42.fr>          +#+  +:+       +#+        */
-// /*                                                +#+#+#+#+#+   +#+           */
-// /*   Created: 2025/05/13 10:43:22 by ajelloul          #+#    #+#             */
-// /*   Updated: 2025/05/17 12:43:03 by ajelloul         ###   ########.fr       */
-// /*                                                                            */
-// /* ************************************************************************** */
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env_manager.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ajelloul <ajelloul@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/13 10:43:22 by ajelloul          #+#    #+#             */
+/*   Updated: 2025/06/13 12:27:43 by ajelloul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-// /*
-// ** Retrieves the value of a specific environment variable
-// ** @param name: Name of the environment variable to find
-// ** @param bash: Pointer to the minibash structure
-// ** @return: Value of the environment variable or NULL if not found
-// */
-// char	*get_environment(const char *name, t_minibash *bash)
-// {
-// 	t_env	*env;
-
-// 	if (!name || !bash)
-// 		return (NULL);
-// 	env = bash->env;
-// 	while (env)
-// 	{
-// 		if (!ft_strcmp(env->name, name))
-// 			return (ft_strdup(env->value));
-// 		env = env->next;
-// 	}
-// 	return (NULL);
-// }
-
-
-void	free_environment_node(t_env **node)
-{
-	free ((*node)->name);
-	free ((*node)->value);
-	(*node)->name = NULL;
-	(*node)->value = NULL;
-	free (*node);
-	*node = NULL;
-}
-
-// /* Node is at the beginning of the list */
-
-void	remove_env_variable(t_env **env, char *var)
-{
-	t_env	*cur;
-	t_env	*prev;
-
-	cur = *env;
-	prev = NULL;
-	while (cur && ft_strcmp(var, cur->name))
-	{
-		prev = cur;
-		cur = cur->next;
-	}
-	if (cur)
-	{
-		if (!prev)
-			*env = cur->next;
-		else if (cur->next)
-			prev->next = cur->next;
-		else
-			prev->next = NULL;
-		free_environment_node(&cur);
-	}
-}
- 
-
- //  ✅   ✅   second   edition ✅ ✅
-
-
-
- int	get_environment_len(t_env	*env)
+int	get_environment_len(t_env *env)
 {
 	t_env	*cur;
 	int		counter;
@@ -93,24 +29,23 @@ void	remove_env_variable(t_env **env, char *var)
 	return (counter);
 }
 
-char *create_env_entry(t_env_converter *conv, t_env *node)
+char	*create_env_entry(t_env_converter *conv, t_env *node)
 {
-    char *entry;
+	char	*entry;
 
-    conv->tmp_name = ft_strdup(node->name);
-    conv->tmp_block = ft_strjoin(node->name, "=");
-    if (node->value)
-    {
-        conv->tmp_value = ft_strdup(node->value);
-        entry = ft_strjoin(conv->tmp_block, conv->tmp_value);
-        free(conv->tmp_value);
-    }
-    else
-        entry = ft_strdup(conv->tmp_block);
-    free(conv->tmp_name);
-    free(conv->tmp_block);
-    
-    return (entry);
+	conv->tmp_name = ft_strdup(node->name);
+	conv->tmp_block = ft_strjoin(node->name, "=");
+	if (node->value)
+	{
+		conv->tmp_value = ft_strdup(node->value);
+		entry = ft_strjoin(conv->tmp_block, conv->tmp_value);
+		free(conv->tmp_value);
+	}
+	else
+		entry = ft_strdup(conv->tmp_block);
+	free(conv->tmp_name);
+	free(conv->tmp_block);
+	return (entry);
 }
 
 t_env	*find_env_variable(t_env *env, char *old)
@@ -131,29 +66,29 @@ void	update_env_var(t_env **env, char **arr_env, char *path, char *old)
 	t_env	*old_pwd;
 	t_env	*pwd;
 
-	old_pwd = *env;
-	pwd = *env;
 	old_pwd = find_env_variable(*env, old);
 	if (old_pwd)
 	{
-		free (old_pwd->value);
+		free(old_pwd->value);
 		old_pwd->value = NULL;
 		if (get_environment_variable(arr_env, "PWD"))
-			old_pwd->value = ft_strdup(get_environment_variable(arr_env, "PWD"));
+			old_pwd->value
+				= ft_strdup(get_environment_variable(arr_env, "PWD"));
 	}
 	pwd = find_env_variable(*env, "PWD");
 	if (pwd)
 	{
-		free (pwd->value);
+		free(pwd->value);
 		pwd->value = NULL;
 		path = getcwd(NULL, 0);
 		if (path)
 		{
 			pwd->value = ft_strdup(path);
-			free (path);
+			free(path);
 		}
 	}
 }
+
 void	free_env_arr(char **arr_env)
 {
 	int	i;
@@ -163,15 +98,10 @@ void	free_env_arr(char **arr_env)
 	i = 0;
 	while (arr_env[i])
 	{
-		if (arr_env[i])
-		{
-			free(arr_env[i]);
-			arr_env[i] = NULL;
-		}
+		free(arr_env[i]);
+		arr_env[i] = NULL;
 		i++;
 	}
 	free(arr_env);
 	arr_env = NULL;
 }
-
-
--- project/src/env/env_parser.c
+++ project/src/env/env_parser.c
@@ -6,18 +6,12 @@
 /*   By: ajelloul <ajelloul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 11:23:33 by ajelloul          #+#    #+#             */
-/*   Updated: 2025/06/11 09:47:37 by ajelloul         ###   ########.fr       */
+/*   Updated: 2025/06/13 12:27:26 by ajelloul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "../../includes/minishell.h"
 
-/*
-** Extracts the environment variable name from a "name=value" string
-** @param str: Environment variable string in format "name=value"
-** @return: Newly allocated string containing just the name portion
-*/
 char	*extract_env_name(const char *str)
 {
 	char	*new;
@@ -41,11 +35,6 @@ char	*extract_env_name(const char *str)
 	return (new);
 }
 
-/*
-** Extracts the environment variable value from a "name=value" string
-** @param str: Environment variable string in format "name=value"
-** @return: Newly allocated string containing just the value portion
-*/
 char	*extract_env_value(const char *str)
 {
 	char	*value_ptr;
@@ -58,11 +47,6 @@ char	*extract_env_value(const char *str)
 	return (ft_strdup(value_ptr + 1));
 }
 
-/*
-** Creates a new environment variable node
-** @param data: Environment variable string in format "name=value"
-** @return: Newly allocated environment node
-*/
 t_env	*create_env_node(char *data)
 {
 	t_env	*new_node;
@@ -102,96 +86,19 @@ void	add_node_to_env(t_env **head, t_env *node)
 	tmp->next = node;
 }
 
-
-void initialize_environment(t_minibash *info, char **env)
+void	initialize_environment(t_minibash *info, char **env)
 {
-    int     i;
-    t_env   *new_node;
+	int		i;
+	t_env	*new_node;
 
-    if (!info || !env)
-        return;
-    i = 0;
-    while (env[i])
-    {
-        new_node = create_env_node(env[i]);
-        if (new_node)
-            add_node_to_env(&info->env, new_node);
-        i++;
-    }
-}
-
-
-// /*
-// ** Creates an empty environment node with just the name no value
-// ** @param name: Name of the environment variable
-// ** @return: Newly allocated environment node
-// */
-// t_env	*create_empty_env_node(char *name)
-// {
-// 	t_env	*new_node;
-
-// 	if (!name)
-// 		return (NULL);
-// 	new_node = (t_env *)malloc(sizeof(t_env));
-// 	if (!new_node)
-// 		return (NULL);
-// 	new_node->name = ft_strdup(name);
-// 	new_node->value = ft_strdup("");
-// 	new_node->next = NULL;
-// 	if (!new_node->name || !new_node->value)
-// 	{
-// 		free(new_node->name);
-// 		free(new_node->value);
-// 		free(new_node);
-// 		return (NULL);
-// 	}
-// 	return (new_node);
-// }
-
-
-
-//  ✅   ✅   ✅    second edition ✅  ✅ ✅   ✅ 
-
-
-char **convert_env_list_to_array(t_env **env)
-{
-    t_env_converter conv;
-    t_env 			*current;
-    int 			i;
-
-    if (!env)
-        return (NULL);
-
-    current = *env;
-    conv.len = get_environment_len(*env);
-    conv.env_array = (char **)malloc(sizeof(char *) * (conv.len + 1));
-    if (!conv.env_array)
-        return (NULL);
-    i = 0;
-    while (i < conv.len)
-    {
-        conv.env_array[i] = create_env_entry(&conv, current);
-        current = current->next;
-        i++;
-    }
-    conv.env_array[i] = NULL;
-    return (conv.env_array);
-}
-
-char	*get_environment_variable(char	**env, char *path)
-{
-	int	i;
-	int	path_len;
-
-	if (!env || !*env || !path)
-		return (NULL);
+	if (!info || !env)
+		return ;
 	i = 0;
-	path_len = ft_strlen(path);
 	while (env[i])
 	{
-		if (!ft_strncmp(env[i], path, path_len) && env[i][path_len] == '=')
-    		return (ft_strchr(env[i], '=') + 1);
+		new_node = create_env_node(env[i]);
+		if (new_node)
+			add_node_to_env(&info->env, new_node);
 		i++;
 	}
-	return (NULL);
 }

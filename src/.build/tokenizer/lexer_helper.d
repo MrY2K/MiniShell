e359e4a7845d53840db6fb6ecbcf028786cb0e48
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-06-27 11:35:23 +0100

    Save my local changes before pulling

---
+++ src/.build/tokenizer/lexer_helper.d
@@ -0,0 +1,11 @@
+src/.build/tokenizer/lexer_helper.o: src/tokenizer/lexer_helper.c \
+  src/tokenizer/../../includes/minishell.h \
+  /goinfre/ajelloul/homebrew/opt/readline/include/readline/readline.h \
+  /goinfre/ajelloul/homebrew/opt/readline/include/readline/rlstdc.h \
+  /goinfre/ajelloul/homebrew/opt/readline/include/readline/rltypedefs.h \
+  /goinfre/ajelloul/homebrew/opt/readline/include/readline/keymaps.h \
+  /goinfre/ajelloul/homebrew/opt/readline/include/readline/chardefs.h \
+  /goinfre/ajelloul/homebrew/opt/readline/include/readline/tilde.h \
+  /goinfre/ajelloul/homebrew/opt/readline/include/readline/history.h \
+  includes/enums.h includes/structs.h includes/minishell.h \
+  includes/prototypes.h includes/../src/lib/libft.h
